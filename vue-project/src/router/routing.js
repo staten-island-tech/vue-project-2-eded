47de71bd49b49eebdd3925af924cdff3ce0544fe
EdisonP2 <EdisonP2@nycstudents.net>
2022-03-23 13:32:02 -0400

    jnjnjn

--- vue-project/src/router/routing.js
+++ vue-project/src/router/routing.js
@@ -8,27 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/signup-page',
-      name: 'signup-page',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/signup-page.vue')
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/login.vue')
     }
   ]
 })
