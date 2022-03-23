26c3579bd0121bbb19fd2e022aa1f1aa161cd5c1
EdisonP2 <EdisonP2@nycstudents.net>
2022-03-23 13:40:05 -0400

    Revert "jnjnjn"
    This reverts commit 47de71bd49b49eebdd3925af924cdff3ce0544fe.

---
+++ vue-project/src/views/signup-page.vue
@@ -0,0 +1,103 @@
+<template>
+  
+        <div class="card-content">
+          <div
+            v-if="validationErrors.length"
+          >
+            <button @click="resetError()" class="delete"></button>
+            <div class="content">
+              Please resolve the following error(s) before proceeding.
+              <ul>
+                <li
+                  v-for="(error, index) in validationErrors"
+                  :key="`error-${index}`"
+                  v-html="error"
+                />
+              </ul>
+            </div>
+          </div>
+            <div>
+              <label>E-mail</label>
+              <div class="control">
+                <input
+                  v-model="email"
+                  class="input"
+                  type="text"
+                  autocomplete="email"
+                  placeholder="example@email.com"
+                />
+              </div>
+            </div>
+            <div>
+              <label >Password</label>
+              <div>
+                <input
+                  v-model="password"
+                  class="input"
+                  type="password"
+                  autocomplete="current-password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div >
+              <label class="label">Repeat password</label>
+              <div class="control">
+                <input
+                  v-model="passwordRepeat"
+                  class="input"
+                  type="password"
+                  autocomplete="new-password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+                <button @click.prevent="validate()">
+                  Register
+                </button>
+        </div>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+      passwordRepeat: null,
+      validationErrors: []
+    };
+  },
+  methods: {
+  
+    validate() {
+              this.validationErrors = [];
+
+      if (!this.email) {
+        this.validationErrors.push("<strong>E-mail</strong> cannot be empty.");
+      }
+      if (/.+@.+/.test(this.email) != true) {
+        this.validationErrors.push("<strong>E-mail</strong> must be valid.");
+      }
+      if (!this.password) {
+        this.validationErrors.push("<strong>Password</strong> cannot be empty");
+      }
+      if (/.{6,}/.test(this.password) != true) {
+        this.validationErrors.push(
+          "<strong>Password</strong> must be at least 6 characters long"
+        );
+      }
+      if (!(this.password === this.passwordRepeat)) {
+        this.validationErrors.push("<strong>Passwords</strong> did not match");
+      }
+      if (this.validationErrors.length <= 0) {
+        this.signUp();
+      }
+    },
+    signUp() {
+      console.log("sign up", this.email, this.password);
+    }
+  }
+};
+</script>
